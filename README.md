585fd2e705e7983c9960e02fb5c6157ad2294bf9
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2024-02-09 22:30:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Displaying Finished Products movement lines of Mrp production
+---------------------------------------------------------
+This module add smart button for the __MRP Production__ finished productions movement lines in the form view.
+
+
+
---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*- 
+
+
+{
+    'name': 'Bypassing the reservation for the Finished Products movements of Mrp production',
+    'author': 'Soft-integration',
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'qweb': [],
+    'description': False,
+    'images': [],
+    'version': '1.0.1',
+    'category': 'Manufacturing/Manufacturing',
+    'demo': [],
+    'depends': ['mrp',],
+    'data': [
+        'views/mrp_production_views.xml'
+    ],
+    'license': 'LGPL-3',
+}
